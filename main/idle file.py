fedb823e5afed152b8e47b1a19024dda4e909027
Alok0202005 <aloklokhande92@gmail.com>
2025-09-09 18:23:49 +0530

    Add files via upload

---
+++ main/idle file.py
@@ -0,0 +1,47 @@
+'''a=[1,3,4,5]
+a.pop(2)
+print(a
+a="VARSHA"
+b=a.lower()
+print(b)
+
+a="varsha"
+b=a.upper()
+print(b)
+
+a="VAR SHA"
+b=a.split()
+print(b)
+
+a=" VARSHA   "
+b=a.strip()
+print(b)
+
+a=" "
+b=a.join(("VARSHA",  "MAURYA"))
+print(b)
+
+a=45
+b=33
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a and b are equal") 
+else:
+    print("a is greater than b")
+
+
+for x in range(60):
+    print(x)
+
+for x in range(6):
+  print (x)
+else:
+    print("Finally finished!")'''
+
+for x in range(6):
+  if x==3:break
+print(x)
+else:
+    print("finally Finished!")
+    
